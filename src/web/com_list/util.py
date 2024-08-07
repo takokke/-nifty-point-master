33874c84795a748669f3b634a797cb2134ab793c
ehmatthes <eric@example.com>
2024-08-07 16:41:18 +0900

    テストhtmlにフィルタフォーム追加

--- src/web/com_list/util.py
+++ src/web/com_list/util.py
@@ -14,3 +14,18 @@ def get_json_data():
     with open(data_path) as f:
         data = json.load(f)
     return data
+
+
+def filter_products_by_int(products, name: str, min: int, max: int):
+    filtered_products = []
+    for product in products:
+        try:
+            value = int(product[name])
+            if min <= value <= max:
+                filtered_products.append(product)
+        except (ValueError, TypeError):
+            # エラーが発生した場合は例外を発生させる
+            raise ValueError(
+                f"Product '{product}' has a non-integer value for '{name}'"
+            )
+    return filtered_products

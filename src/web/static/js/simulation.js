3aa033190c1fd2a15d6e816462cd06ab4220850d
m39hira <m807039c@mails.cc.ehime-u.ac.jp>
2024-08-07 14:47:31 +0900

    入力フォーム、ハイライト作成

--- src/web/static/js/simulation.js
+++ src/web/static/js/simulation.js
@@ -1,6 +1,8 @@
 const ctx = document.getElementById("lineChart")
 const currentPointInput = document.getElementById("current-point")
 const monthlyGetPointInput = document.getElementById("monthly-get-point")
+const goalPointInput = document.getElementById("goal-point")
+
 
 
 const getAdjustedMonth = (month) => {
@@ -11,14 +13,10 @@ const getAdjustedMonth = (month) => {
 const today = new Date()
 const thisMonth = today.getMonth() + 1
 
-
-// 目標ポイント
-const goalPoint = 2500
-const goalPointChart = Array(6).fill(goalPoint)
-
 // 初期状態のグラフ
 const initPoint = 0
 const initPointChart = Array(6).fill(initPoint)
+const initGoalPoint = 0
 
 // 今、1、3、半年、1年後
 
@@ -34,14 +32,6 @@ let myLineChart = new Chart(ctx, {
             // getAdjustedMonth(thisMonth + 5)
         ],
         datasets: [
-       {
-            label: '目標',
-            type: 'line',
-            data: goalPointChart,
-            pointRadius: 0,
-            borderColor: "rgba(255,0, 0, 1)",
-            backgroundColor: "rgba(0,0,0,0)",
-        },
         {
             label: '獲得予定ポイント',
             type: 'bar',
@@ -73,19 +63,47 @@ let myLineChart = new Chart(ctx, {
             text: 'ポイント獲得予定'
         },
         scales: {
-        yAxes: [{
+            yAxes: [{
+            id: 'goal',
             ticks: {
             suggestedMax: 5000,
             suggestedMin: 0,
             callback: function(value, index, values){
                 return  value +  ''
             }
-            }
-            }]
-            
+        }
+        }]    
         },
         legend: {
             display: false // ラベルを非表示にする
+        },
+        annotation: {
+            annotations: [
+            {
+                type: 'line', // 線分を指定
+                drawTime: 'afterDatasetsDraw',
+                mode: 'horizontal', // 水平を指定
+                scaleID: 'goal', // 基準とする軸のid名
+                value: initGoalPoint, // 引きたい線の数値（始点）
+                endValue: initGoalPoint, // 引きたい線の数値（終点）
+                borderColor: 'red', // 線の色
+                borderWidth: 3, // 線の幅（太さ）
+                label: { // ラベルの設定
+                    backgroundColor: 'rgba(255,255,255,1)',
+                    bordercolor: 'rgba(200,60,60,0.8)',
+                    borderwidth: 2,
+                    fontStyle: 'normal',
+                    fontColor: 'rgba(0,0,0,1)',
+                    xPadding: 10,
+                    yPadding: 10,
+                    cornerRadius: 3,
+                    position: 'right',
+                    xAdjust: -10,
+                    yAdjust: -20,
+                    enabled: true,
+                    content: '目標ポイント'
+                }
+            }]
         }
     }
 })
@@ -96,7 +114,7 @@ currentPointInput.addEventListener("input", () => {
     const monthlyPoint = monthlyGetPointInput.value;
 
     // myLineChart.data.datasets[1].data = [0, 600, 1200, 1800, 2400, 3000, 3600].map((val, idx) => Number(monthlyPoint)* idx + Number(currentPoint));
-    myLineChart.data.datasets[1].data = [0, 0, 0, 0, 0].map((val, idx) => {
+    myLineChart.data.datasets[0].data = [0, 0, 0, 0, 0].map((val, idx) => {
         let updateVal = 0;
         switch (idx) {
         case 0:
@@ -125,7 +143,7 @@ monthlyGetPointInput.addEventListener("input", () => {
     const currentPoint = currentPointInput.value;
 
     // myLineChart.data.datasets[1].data = [0, 600, 1200, 1800, 2400, 3000, 3600].map((val, idx) => Number(monthlyPoint)* idx + Number(currentPoint));
-    myLineChart.data.datasets[1].data = [0, 0, 0, 0, 0].map((val, idx) => {
+    myLineChart.data.datasets[0].data = [0, 0, 0, 0, 0].map((val, idx) => {
         let updateVal = 0;
         switch (idx) {
         case 0:
@@ -146,6 +164,14 @@ monthlyGetPointInput.addEventListener("input", () => {
         }
         return updateVal
     })
+    myLineChart.update()
+
+})
+goalPointInput.addEventListener("input", () => { 
+    const goalPoint = goalPointInput.value;
+    
+    myLineChart.options.annotation.annotations[0].value  =  Number(goalPoint);
+    myLineChart.options.annotation.annotations[0].endValue  =  Number(goalPoint);
     myLineChart.update()
 
 })

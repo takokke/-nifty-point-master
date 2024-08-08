97d89b6cba00c607f58a23325fcc0c8c46eed5a0
kasamura <kasamura@mtl.t.u-tokyo.ac.jp>
2024-08-08 17:07:45 +0900

    Admin wip

--- src/web/admin/routes.py
+++ src/web/admin/routes.py
@@ -15,8 +15,6 @@ def admin():
     if not current_user.is_admin:
         return redirect(url_for("app.profile.profile"))
     users = User.query.all()
-    me=current_user
-    username=me.username
     data = [
         {
             "id": user.id,
@@ -32,14 +30,32 @@ def admin():
     return render_template("admin.html", data=data)
 
 
-@ADMIN_BP.route("/delete/<user_id>", methods=["POST"])
+@ADMIN_BP.route("/update", methods=["GET", "POST"])
 @login_required
-def delete(user_id):
+def update():
     if not current_user.is_admin:
-        return render_template("user.html"), 403
+        return redirect(url_for("app.update.update"))
 
-    user = User.query.filter_by(id=user_id).first()
-    if user and user.id != current_user.id:
-        DB.session.delete(user)
-        DB.session.commit()
-    return redirect(url_for("app.admin.admin"))
+    if request.method == "POST":
+        data = {
+            "id": request.form.get("id"),
+            "username": request.form.get("username"),
+            "current_point": request.form.get("current_point"),
+            "monthly_point": request.form.get("monthly_point"),
+            "goal_point": request.form.get("goal_point"),
+        }
+
+        response = request.put(url_for("app.api.update_user"), json=data)
+        if response.status_code == 200:
+            flash("更新しました", "success")
+        else:
+            flash("更新に失敗しました", "danger")
+
+        return redirect(url_for("app.admin.admin"))
+
+    elif request.method == "GET":
+        user_id = request.args.get("user_id")
+        user = User.query.get(user_id)
+        if user and user.id != current_user.id:
+            return render_template("update.html", user=user)
+        return redirect(url_for("app.admin.admin"))

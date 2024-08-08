51801ad2e2c9b3116ab2f57d9ba1a44cf655e444
kasamura <kasamura@mtl.t.u-tokyo.ac.jp>
2024-08-08 11:49:51 +0900

    Change top page and modify login page

--- src/web/routes.py
+++ src/web/routes.py
@@ -1,17 +1,14 @@
 import logging
 
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from web.admin.routes import ADMIN_BP
 from web.auth.routes import AUTH_BP
-
+from web.com_list.routes import COMLIST_BP
 from web.profile.routes import PROFILE_BP
 from web.simulation.routes import SIMULATION_BP
 from web.update.routes import UPDATE_BP
-
-from web.com_list.routes import COMLIST_BP
-
 
 APP_BP = Blueprint("app", __name__)
 
@@ -26,12 +23,12 @@ APP_BP.register_blueprint(SIMULATION_BP)
 APP_BP.register_blueprint(COMLIST_BP)
 
 
-
 @APP_BP.route("/")
 def index():
     # ログ出力の方法
     logging.debug("トップページにアクセスされました")
-    return render_template("index.html")
+    # return render_template("index.html")
+    return redirect(url_for("app.simulation.simulation"))
 
 
 @APP_BP.route("/secret")
